6cec4115861c5eef21c308a59d4ea28d04b31510
khalaeva <121284348+khalaeva@users.noreply.github.com>
2023-03-20 14:38:08 +0300

    btstrp

--- vite-project/src/components/Header.vue
+++ vite-project/src/components/Header.vue
@@ -1,36 +1,17 @@
 <template>
- <header class="header">
-    <div class="header-catalog">
-        Catalog
-    </div>
-    <div class="header-links">
-        <div class="header-links__user">
-            User
-        </div>
-        <div class="header-links__cart">
-            Cart
-        </div>
-    </div>
-</header>
+    <header class="d-flex justify-content-center py-3">
+        <ul class="nav nav-pills">
+          <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+        </ul>
+      </header>
 </template>
 
-<script>
-
+<script setup>
 </script>
 
-<style lang="scss" scoped>
-.header {
-    width: 100%;
-    height: 120px;
-	background: #D8E3AB;
-    position: fixed; 
-    &-links {
-        list-style: none;
-        position: absolute;
-        top: 10px;
-        right: 100px;
-    }
-}
-
-
+<style lang="scss">
 </style>
