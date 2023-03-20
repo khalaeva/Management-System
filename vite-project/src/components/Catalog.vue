b0df9893d00245584fe14dd2457e733b04710d3f
khalaeva <121284348+khalaeva@users.noreply.github.com>
2023-03-20 23:52:04 +0300

    header

---
+++ vite-project/src/components/Catalog.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="wrapper"> 
+        <div class="red">1</div> 
+        <div class="green">2</div> 
+        <div class="blue">3</div> 
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+    .wrapper { 
+        display: flex; 
+        flex-wrap: wrap; 
+        background-color: beige; 
+        height: 100vh; 
+    } 
+    .wrapper > div { 
+        padding: 10px; 
+        width: 100%; 
+        font-size: 4vw; 
+        color: white; 
+    } 
+    .red { 
+        background: orangered; 
+    } 
+    .green { 
+        background: yellowgreen; 
+    }
+    .blue { 
+        background: steelblue; 
+    } 
+    body { 
+        margin: 0; 
+    }
+</style>
