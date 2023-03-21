29bc59a345ed5f08d6523b102183cd93ed622b63
khalaeva <121284348+khalaeva@users.noreply.github.com>
2023-03-21 15:07:52 +0300

    cmt

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Header from './components/Header.vue'
+import Catalog from './components/Catalog.vue';
 </script>
 
 <template>
   <Header/>
+  <Catalog/>
 </template>
 
 <style scoped>
--- vite-project/src/components/Header.vue
+++ vite-project/src/components/Header.vue
@@ -1,17 +1,85 @@
 <template>
-    <header class="d-flex justify-content-center py-3">
-        <ul class="nav nav-pills">
-          <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-          <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-          <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-          <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-          <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-        </ul>
-      </header>
+    <header class="header">
+        <div class="header-top">
+            <div class="header-top-search">
+                <img src="/svg/menu.svg" alt="menu">
+            </div>
+            <a href="#" class="header-logo">Online Shop</a>
+            <div class="header-top-right">
+                <div class="header-top-right__cart">
+                    <a href="#">
+                        <img src="/svg/cart.svg" alt="cart">
+                    </a>
+                </div>
+                <div class="header-top-right__profile">
+                    <a href="#">
+                        <img src="/svg/user.svg" alt="user">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="header-menu">
+            <router-link 
+                class="header-menu__link"
+                :to="element.path" 
+                v-for="(element, i) of menu"
+                :key="i">{{ element.name }}</router-link>
+        </div>
+    </header>
 </template>
 
 <script setup>
+
 </script>
 
 <style lang="scss">
+img {
+    height: 40px;
+    width: 40px;
+}
+.header {
+    height: 132px;
+    background: #fff;
+    &-top {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        height: 70px;
+        align-items: center;
+        border-bottom: 1px solid rgba(0, 0, 0, 1);
+        margin: 0 28px;
+        &-right {
+            display: flex;
+            align-items: center;
+            justify-content: flex-end;
+            &__cart {
+                margin-right: 16px;
+            }
+        }
+    }
+    &-logo{
+        color: black;
+        font-size: 24px;
+        text-decoration: none;
+        font-family: var(--clash);
+        text-align: center;
+        &:hover {
+            text-decoration: underline;
+            color: black;
+        }
+    }
+    &-menu {
+        height: 62px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        &__link {
+            margin: 0 22px;
+            color: #726E8D;
+            text-decoration: none;
+            &:hover {
+                text-decoration: underline;
+            }
+        }
+    }
+}
 </style>
