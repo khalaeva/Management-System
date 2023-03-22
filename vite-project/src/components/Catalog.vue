b25adb14ced69d9d1e46a4254d54750f0367095d
Anna <halaevaanna@gmail.com>
2023-03-22 13:47:43 +0300

    <a>

--- vite-project/src/components/Catalog.vue
+++ vite-project/src/components/Catalog.vue
@@ -1,46 +1,52 @@
 <template>
-    <div class="product"> 
-        <img class="product__image" src="/img/1.jpeg">
-        <div class="product-about">
-            <p>Name of product</p>
-            <p>About product sdfsflsdfkal sdflsdhf sdjfh dflskdjf clvh</p>
-        </div>
-        <div class="product-status">
-            <p>In stock</p>
-        </div>
-        <div class="product-price">
-            <p>1000 $</p>
-            <button type="button" class="btn btn-primary">В корзину</button>
-        </div>
-    </div>
-    <div class="product"> 
-        <img class="product__image" src="/img/2.jpeg">
-        <div class="product-about">
-            <p>Name of product</p>
-            <p>About product sdfsflsdfkal sdflsdhf sdjfh dflskdjf clvh</p>
-        </div>
-        <div class="product-status">
-            <p>In stock</p>
-        </div>
-        <div class="product-price">
-            <p>1000 $</p>
-            <button type="button" class="btn btn-primary">В корзину</button>
-        </div>
-    </div>
-    <div class="product"> 
-        <img class="product__image" src="/img/3.jpeg">
-        <div class="product-about">
-            <p>Name of product</p>
-            <p>About product sdfsflsdfkal sdflsdhf sdjfh dflskdjf clvh</p>
-        </div>
-        <div class="product-status">
-            <p>In stock</p>
-        </div>
-        <div class="product-price">
-            <p>1000 $</p>
-            <button type="button" class="btn btn-primary">В корзину</button>
-        </div>
-    </div>
+    <a class="product-link" href="#">
+        <span class="product"> 
+            <img class="product__image" src="/img/1.jpeg">
+            <div class="product-about">
+                <p>Name of product</p>
+                <p>About product sdfsflsdfkal sdflsdhf sdjfh dflskdjf clvh</p>
+            </div>
+            <div class="product-status">
+                <p>In stock</p>
+            </div>
+            <div class="product-price">
+                <p>1000 $</p>
+                <button type="button" class="btn btn-primary">В корзину</button>
+            </div>
+        </span>
+    </a>
+    <a class="product-link" href="#">
+        <span class="product"> 
+            <img class="product__image" src="/img/1.jpeg">
+            <div class="product-about">
+                <p>Name of product</p>
+                <p>About product sdfsflsdfkal sdflsdhf sdjfh dflskdjf clvh</p>
+            </div>
+            <div class="product-status">
+                <p>In stock</p>
+            </div>
+            <div class="product-price">
+                <p>1000 $</p>
+                <button type="button" class="btn btn-primary">В корзину</button>
+            </div>
+        </span>
+    </a>
+    <a class="product-link" href="#">
+        <span class="product"> 
+            <img class="product__image" src="/img/1.jpeg">
+            <div class="product-about">
+                <p>Name of product</p>
+                <p>About product sdfsflsdfkal sdflsdhf sdjfh dflskdjf clvh</p>
+            </div>
+            <div class="product-status">
+                <p>In stock</p>
+            </div>
+            <div class="product-price">
+                <p>1000 $</p>
+                <button type="button" class="btn btn-primary">В корзину</button>
+            </div>
+        </span>
+    </a>
 </template>
 
 <script setup>
@@ -48,6 +54,10 @@
 </script>
 
 <style lang="scss" scoped>
+    .product-link {
+        text-decoration: none;
+        color: black;
+    }
     .product { 
         display: flex; 
         margin-bottom: 10px;
@@ -69,4 +79,8 @@
             vertical-align: middle;
         }
     } 
+    .product:hover {
+        background: rgba(0,0,0,.05);
+    }
+
 </style>
