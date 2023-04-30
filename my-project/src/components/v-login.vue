14d89f69b1d1239668ff77bb7ecc889688e47f68
khalaeva <121284348+khalaeva@users.noreply.github.com>
2023-04-30 13:14:29 +0300

    login

--- my-project/src/components/v-login.vue
+++ my-project/src/components/v-login.vue
@@ -1,37 +1,43 @@
 <template>
-    <div class="user_login" >
+    <div class="v-layout-header">
+        <div class="v-layout-header-left">
+            <img class="logo" src="../../public/svg/free-icon-process-4149677.png">
+            <h1>MY business</h1>
+        </div>
+    </div>
+    <div class="user_login">
         <form>
-            <h4 class="user_login__i">Log in</h4>
+            <h4 class="user_login__i">Вход</h4>
             <div class="user_login__i form-group">
-              <label for="exampleInputEmail1">Email address</label>
+              <label for="exampleInputEmail1">Введите свой E-mail</label>
               <input 
                 type="email" 
                 class="form-control" 
                 id="exampleInputEmail1" 
                 aria-describedby="emailHelp" 
-                placeholder="Enter email"
+                placeholder="E-mail"
                 v-model="login.name">
-              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                <small style="color:red" v-if="errors[0]">{{ errors[0] }}!</small>
             </div>
             <div class="user_login__i form-group">
-              <label for="exampleInputPassword1">Password</label>
+              <label for="exampleInputPassword1">Введите свой пароль</label>
               <input 
                 type="password" 
                 class="form-control" 
                 id="exampleInputPassword1" 
-                placeholder="Password"
+                placeholder="Пароль"
                 v-model="login.password">
+                <small style="color:red" v-if="errors[1]">{{ errors[1] }}!</small>
             </div>
-            <RouterLink to="/analysis">
-                <button @click="addLogin" class="btn btn-primary" style="margin-bottom: 10px">Log In</button>
-            </RouterLink>
+                <button type="button" @click="checkForm" class="btn btn-secondary" style="margin-bottom: 10px">Log In</button>
             <br>
         </form>
     </div>
     </template>
     
     <script>
-    import axios from 'axios'
+    import router from '@/router/router'
+import axios from 'axios'
 
     export default {
         name: 'v-login',
@@ -40,20 +46,60 @@
                 login: {
                     name: '',
                     password: ''
-                }
+                },
+                errors: []
             }
         },
         methods: {
-            addLogin() {
-                axios.post('http://localhost:3000/login', this.login)
+            checkForm() {
+                if (this.login.name && this.login.password) {
+                    axios.post('http://localhost:3000/login', this.login)
+                    router.push("/analysis")
+                }
+                this.errors = [];
+                if (!this.login.name) {
+                    this.errors.push('Требуется указать E-mail')
+                } else if (!this.validEmail(this.login.name)) {
+                    this.errors.push('Укажите корректный E-mail')
+                }
+
+                if (!this.login.password) {
+                    this.errors.push('Требуется указать пароль')
+                }
             },
+            validEmail(email) {
+                var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                return re.test(email);
+            }
         },
         
     }
     </script>
     
     <style lang="scss" scoped>
+    .logo {
+        width: 50px;
+        height: 50px;
+    }
+    .v-layout-header {
+        display: flex;
+        height: 100px;
+        justify-content: space-around;
+        align-items: center;
+        background-color: lightgray;
+        margin-bottom: 30px;
+        &-left {
+            display: grid;
+            grid-template-columns: 1fr 3fr;
+        }
+        &-right {
+            width: 30%;
+            display: flex;
+            justify-content: space-around;
+        }
+    }
     .user_login{
+        width: 500px;
         padding: 50px;
         border: 1px solid lightgrey;
         position: fixed; top: 50%; left: 50%;
